6bbb6ba0d23f977043c6372bf232aaebe319265a
Justin Merryman <justinmerryman@yahoo.com>
2021-03-07 14:14:48 -0600

    updated barchart

--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,44 @@
-function defaultValue (){
+function defaultValue() {
     var selector = d3.selectAll('#selDataset');
 
-    d3.json('samples.json').then((data)=>{
-    
-        var sampleNames=data.names;
+    d3.json('samples.json').then((data) => {
+
+        var sampleNames = data.names;
         sampleNames.forEach((sample) => {
             selector
-              .append("option")
-              .text(sample)
-              .property("value", sample);
-            });
-      
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
         var defaultID = sampleNames[0];
-    
+
         barChart(defaultID);
         bubbleChart(defaultID);
         metaData(defaultID);
         gaugeChart(defaultID)
+    })
+};
 
-      });
-     
-    };
-
-    function optionChanged(newID){
-        barChart(newID);
-        bubbleChart(newID);
-        metaData(newID);
-    };
+function optionChanged(newID) {
+    barChart(newID);
+    bubbleChart(newID);
+    metaData(newID);
+};
 
 
 defaultValue();
 
 
-function barChart(subjectID){
+function barChart(subjectID) {
 
-    d3.json('samples.json').then((data) =>{
+    d3.json('samples.json').then((data) => {
         var samples = data.samples;
         var ID = samples.map(row => row.id).indexOf(subjectID);
-        var otuValue = samples.map(row =>row.sample_values);
-        var otuValueTen = otuValue[ID].slice(0,10).reverse();
+        var otuValue = samples.map(row => row.sample_values);
+        var otuValueTen = otuValue[ID].slice(0, 10).reverse();
         var otuID = samples.map(row => row.otu_ids);
-        var otuIDTen = otuID[ID].slice(0,10);
-        var otuLabel = samples.map(row => row.otu_labels).slice(0,10);
+        var otuIDTen = otuID[ID].slice(0, 10);
+        var otuLabel = samples.map(row => row.otu_labels).slice(0, 10);
         console.log(subjectID)
         console.log(ID)
         console.log(otuLabel)
@@ -54,19 +51,19 @@ function barChart(subjectID){
             orientation: 'h'
 
         }
-        var layout = {title: "OTUs Found", margin: { t: 30, l: 150 }};
+        var layout = { title: "OTUs Found", margin: { t: 30, l: 150 } };
         Plotly.newPlot('bar', [trace1], layout);
     })
 };
 
-function bubbleChart(subjectID){
+function bubbleChart(subjectID) {
 
-    d3.json('samples.json').then((data) =>{
+    d3.json('samples.json').then((data) => {
         var samples = data.samples;
         var ID = samples.map(row => row.id).indexOf(subjectID);
         var otuID = samples.map(row => row.otu_ids);
         var otuIDs = otuID[ID];
-        var otuValue = samples.map(row =>row.sample_values);
+        var otuValue = samples.map(row => row.sample_values);
         var otuValues = otuValue[ID];
         var otuLabel = samples.map(row => row.otu_labels);
         var otuLabels = otuLabel[ID];
@@ -76,18 +73,18 @@ function bubbleChart(subjectID){
             y: otuValues,
             text: otuLabels,
             mode: 'markers',
-            marker : { size: otuValues, color: otuIDs}
+            marker: { size: otuValues, color: otuIDs }
         };
-        var layout = {xaxis: {title: 'OTU ID'}};
+        var layout = { xaxis: { title: 'OTU ID' } };
 
         Plotly.newPlot('bubble', [trace2], layout);
 
-        })
+    })
 }
 
-function metaData(subjectID){
+function metaData(subjectID) {
 
-    d3.json('samples.json').then((data)=>{
+    d3.json('samples.json').then((data) => {
         var metadata = data.metadata;
         var searchData = metadata.filter(item => item.id == subjectID);
         var outputData = searchData[0];
@@ -98,7 +95,7 @@ function metaData(subjectID){
         //clear panel
         panel.html("")
 
-        Object.entries(outputData).forEach(([key, value])=> {
+        Object.entries(outputData).forEach(([key, value]) => {
 
             panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
 
