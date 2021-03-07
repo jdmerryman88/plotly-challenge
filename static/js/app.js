22d66ed3e4b4db510c7f72212b1ede6e6f1d2b9c
Justin Merryman <justinmerryman@yahoo.com>
2021-03-07 14:15:17 -0600

    updated barchart

--- static/js/app.js
+++ static/js/app.js
@@ -40,7 +40,7 @@ function barChart(subjectID) {
         var otuIDTen = otuID[ID].slice(0, 10);
         var otuLabel = samples.map(row => row.otu_labels).slice(0, 10);
         //console.log(subjectID)
-        console.log(ID)
+        //console.log(ID)
         console.log(otuLabel)
 
         let trace1 = {
